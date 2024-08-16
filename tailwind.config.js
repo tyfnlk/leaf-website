b804694312f04100db3932c6d037fd8a2cc40a96
Terry Lee <93066898+tyfnlk@users.noreply.github.com>
2024-08-16 10:58:39 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'pages/**/*.{ts,tsx}'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1280px',
+        '2xl': '1400px'
+      },
+      colors: {
+        'primary': '#575C12',
+        'background': '#FCF8F0',
+        // 'purple': '#7e5bef',
+        // 'pink': '#ff49db',
+        // 'orange': '#ff7849',
+        // 'green': '#13ce66',
+        // 'yellow': '#ffc82c',
+        // 'gray-dark': '#273444',
+        // 'gray': '#8492a6',
+        // 'gray-light': '#d3dce6',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      fontFamily: {
+        // sans: ['var(--font-sans)', ...fontFamily.sans]
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
+  },
+  plugins: [require('tailwindcss-animate')]
+};
